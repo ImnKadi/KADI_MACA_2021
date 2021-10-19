cb91efd6e15af18912ae160330689ea782ccb511
Imane Kadi <imanemnds@gmail.com>
2021-10-19 12:28:29 +0200

    Classe Repertoire (non finie) et main (erreurs à débuguer)

--- TD2/app/src/main/java/TD2/Components/Repertoire.java
+++ TD2/app/src/main/java/TD2/Components/Repertoire.java
@@ -1,8 +1,44 @@
 package TD2.Components;
 
-public class Repertoire {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Repertoire extends ComposantImpl implements Composant, Composite<Composant>{
+
+    private List<Composant> getChildren;
+
+    public Repertoire(String name, Owner owner) {
+        super(name, owner);
+        getChildren = new ArrayList<>();
+    }
 
 
+    @Override
+    public void appendContent(String name) {
+    }
 
+    @Override
+    public boolean isComposite() {
+        return false;
+    }
 
+    @Override
+    public List<Composant> getChildren() {
+        return null;
+    }
+
+    @Override
+    public void addChild() {
+
+    }
+
+    @Override
+    public boolean removechild(Composant composant) {
+        return false;
+    }
+
+    @Override
+    public boolean removeChildren(List<Composant> t) {
+        return false;
+    }
 }
